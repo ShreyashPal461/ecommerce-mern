48163ec6a832a7988c118a34fa59727f1295838b
shreyash <shreyashpal461@gmail.com>
2026-03-18 22:53:40 +0530

    fix vercel

--- frontend/src/Config/Api.ts
+++ frontend/src/Config/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-export const DEPLOYED_URL = import.meta.env.VITE_DEPLOYED_URL || "https://zosh-bazaar-mern.onrender.com";
+export const DEPLOYED_URL = import.meta.env.VITE_DEPLOYED_URL || "https://ecommerce-mern-fccj.onrender.com";
 
 // Use deployed URL for production, localhost for development
 const isDevelopment = import.meta.env.DEV;
